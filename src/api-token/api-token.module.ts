f04ddd734328af5fd0fe7247a3f47a456973a8c1
juandirodrib12 <141871531+juandirodrib12@users.noreply.github.com>
2025-11-27 06:57:00 -0500

    moudlo api token completamente listo

--- src/api-token/api-token.module.ts
+++ src/api-token/api-token.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ApiTokenService } from './api-token.service';
 import { ApiTokenController } from './api-token.controller';
+import { ApiTokenGuard } from 'src/guards/api-token/api-token.guard';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApiToken } from './entities/api-token.entity';
 
 @Module({
-  controllers: [ApiTokenController],
+  controllers: [ApiTokenController, ApiTokenGuard],
   providers: [ApiTokenService],
+  imports: [
+    TypeOrmModule.forFeature([ApiToken])
+  ],
+  exports: [ApiTokenService, ApiTokenGuard],
+
 })
 export class ApiTokenModule {}
