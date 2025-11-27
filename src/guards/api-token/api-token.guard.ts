148e09fa0073fc2e83f666f3a95cc599a79e2c96
juandirodrib12 <141871531+juandirodrib12@users.noreply.github.com>
2025-11-27 07:00:15 -0500

    api token listo para usarse

--- src/guards/api-token/api-token.guard.ts
+++ src/guards/api-token/api-token.guard.ts
@@ -1,11 +1,23 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { ApiTokenService } from 'src/api-token/api-token.service';
 
 @Injectable()
 export class ApiTokenGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+
+  constructor(private readonly apiTokenService: ApiTokenService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const apiToken = request.headers['api-token'];
+
+    if (!apiToken) {
+      throw new UnauthorizedException('API token is missing');
+    }
+
+    const isValid = await this.apiTokenService.isActive(apiToken);
+    if (!isValid) {
+      throw new UnauthorizedException('Invalid API token');
+    }
     return true;
   }
 }
