b9cd091bf26d5fa8f3aa8d960cacf522fddfae37
juandirodrib12 <141871531+juandirodrib12@users.noreply.github.com>
2025-11-27 07:46:16 -0500

    servicios listo

--- src/location/dto/create-location.dto.ts
+++ src/location/dto/create-location.dto.ts
@@ -1,1 +1,16 @@
-export class CreateLocationDto {}
+import { IsNumber, IsString } from "class-validator";
+
+export class CreateLocationDto {
+
+    @IsString()
+    name: string;
+
+    @IsString()
+    type: string;
+
+    @IsNumber()
+    cost: number;
+
+    @IsNumber()
+    ownerId: number;
+}
--- src/location/location.service.ts
+++ src/location/location.service.ts
@@ -1,26 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { CreateLocationDto } from './dto/create-location.dto';
 import { UpdateLocationDto } from './dto/update-location.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Location } from './entities/location.entity';
+import { Character } from 'src/character/entities/character.entity';
 
 @Injectable()
 export class LocationService {
-  create(createLocationDto: CreateLocationDto) {
-    return 'This action adds a new location';
+
+  constructor(
+
+    @InjectRepository(Location)
+    private locationRepository: Repository<Location>,
+
+    @InjectRepository(Character)
+    private characterRepository: Repository<Character>,
+
+  ) {}
+
+  async create(createLocationDto: CreateLocationDto) {
+    const owner = await this.characterRepository.findOne({ where: { id: createLocationDto.ownerId } });
+    if (!owner) {
+      throw new Error('Character not found');
+    }
+    const newLocation = this.locationRepository.create({
+      ...createLocationDto,
+      owner,
+    });
+    return this.locationRepository.save(newLocation);
   }
 
-  findAll() {
-    return `This action returns all location`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} location`;
-  }
-
-  update(id: number, updateLocationDto: UpdateLocationDto) {
-    return `This action updates a #${id} location`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} location`;
+  async findAll() {
+    return await this.locationRepository.find({ relations: { favoriteCharacters: true } });
   }
 }
