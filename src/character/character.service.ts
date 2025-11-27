b9b7d9eb8dd6b84f88761c6b3c42dca11449b00b
juandirodrib12 <141871531+juandirodrib12@users.noreply.github.com>
2025-11-27 07:37:24 -0500

    servicio characters listo

--- src/character/character.service.ts
+++ src/character/character.service.ts
@@ -1,26 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCharacterDto } from './dto/create-character.dto';
 import { UpdateCharacterDto } from './dto/update-character.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Character } from './entities/character.entity';
+import { In, Repository } from 'typeorm';
+import { Location } from 'src/location/entities/location.entity';
 
 @Injectable()
 export class CharacterService {
-  create(createCharacterDto: CreateCharacterDto) {
-    return 'This action adds a new character';
+
+  constructor(
+
+    @InjectRepository(Character)
+    private characterRepository: Repository<Character>,
+
+    @InjectRepository(Location)
+    private locationRepository: Repository<Location>,
+
+  ) {}
+
+  async create(createCharacterDto: CreateCharacterDto) {
+    const newCharacter = await this.characterRepository.create(createCharacterDto);
+    return this.characterRepository.save(newCharacter);
   }
 
-  findAll() {
-    return `This action returns all character`;
+  async addFavorite(characterId: number, locationId: number) {
+    const character = await this.characterRepository.findOne({ where: { id: characterId }, relations: { favoriteLocations: true } });
+    const location = await this.locationRepository.findOne({ where: { id: locationId } });
+    if (!character) {
+      throw new NotFoundException('Character not found');
+    }
+    if (!location) {
+      throw new NotFoundException('Location not found');
+    }
+    character.favoriteLocations.push(location);
+    await this.characterRepository.save(character);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} character`;
-  }
-
-  update(id: number, updateCharacterDto: UpdateCharacterDto) {
-    return `This action updates a #${id} character`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} character`;
+  async calculateTaxes(id: number) {
+    const character = await this.characterRepository.findOne({ where: { id }, relations: { location: true } });
+    if (!character) {
+      throw new NotFoundException('Character not found');
+    }
+    if (!character.location) {
+      return 0;
+    }
+    if (character.employee) {
+      return character.location.cost * (1+0.08);
+    }
+    return character.location.cost*(1+0.03);
   }
 }
