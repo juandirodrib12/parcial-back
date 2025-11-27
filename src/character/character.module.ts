f9c47ec3ae009d5ffc8e78d1201cc4b71d61f275
juandirodrib12 <141871531+juandirodrib12@users.noreply.github.com>
2025-11-27 07:18:35 -0500

    importaciones modulo

--- src/character/character.module.ts
+++ src/character/character.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { CharacterService } from './character.service';
 import { CharacterController } from './character.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Character } from './entities/character.entity';
+import { Location } from 'src/location/entities/location.entity';
+import { ApiTokenModule } from 'src/api-token/api-token.module';
 
 @Module({
   controllers: [CharacterController],
   providers: [CharacterService],
+  imports: [
+    TypeOrmModule.forFeature([Character, Location]),
+    ApiTokenModule
+  ],
+  exports: [CharacterService],
 })
 export class CharacterModule {}
--- src/location/location.module.ts
+++ src/location/location.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { LocationService } from './location.service';
 import { LocationController } from './location.controller';
+import { Character } from 'src/character/entities/character.entity';
+import { Location } from './entities/location.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApiTokenModule } from 'src/api-token/api-token.module';
 
 @Module({
   controllers: [LocationController],
   providers: [LocationService],
+  imports: [
+    TypeOrmModule.forFeature([Location, Character]),
+    ApiTokenModule
+  ],
+  exports: [LocationService],
 })
 export class LocationModule {}
