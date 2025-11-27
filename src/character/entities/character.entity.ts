2370224ead9e3da61215973937507e32f6b1182e
juandirodrib12 <141871531+juandirodrib12@users.noreply.github.com>
2025-11-27 07:11:38 -0500

    creacion entidades

--- src/character/entities/character.entity.ts
+++ src/character/entities/character.entity.ts
@@ -1,1 +1,26 @@
-export class Character {}
+import { Location } from "src/location/entities/location.entity";
+import { Column, Entity, JoinColumn, JoinTable, ManyToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Character {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column('text')
+    name: string;
+
+    @Column('number')
+    salary: number;
+
+    @Column('boolean')
+    employee: boolean;
+
+    @OneToOne(() => Location, location => location.owner, { cascade: true })
+    @JoinColumn()
+    location: Location;
+
+    @ManyToMany(() => Location, location => location.favoriteCharacters, { cascade: true })
+    @JoinTable()
+    favoriteLocations: Location[];
+}
--- src/location/entities/location.entity.ts
+++ src/location/entities/location.entity.ts
@@ -1,1 +1,24 @@
-export class Location {}
+import { Character } from "src/character/entities/character.entity";
+import { Column, Entity, JoinColumn, JoinTable, ManyToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Location {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column('text')
+    name: string;
+
+    @Column('text')
+    type: string;
+
+    @Column('number')
+    cost: number;
+
+    @OneToOne(() => Character, character => character.location)
+    owner: Character;
+
+    @ManyToMany(() => Character, character => character.favoriteLocations)
+    favoriteCharacters: Character[];
+}
